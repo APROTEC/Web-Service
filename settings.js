615bbd7bbbc9fd42aec9672e7ccbf1101ed77620
ldrc2895 <ldrc2895@gmail.com>
2016-04-24 04:51:35 -0600

    Varas que se me olvidaron

--- settings.js
+++ settings.js
@@ -1,7 +1,13 @@
 ﻿exports.dbConfig  = {
-	user :"sa",
-	password : "ld1995",
-	server : "localhost\\MSSQLSERVER",
+	user :"admin_server@aprotec-server",
+	password : "Aprotec2016",
+	server : "aprotec-server.database.windows.net",
 	database : "aprotecBD",
-	port : 1433
+	port : 1433,
+	 options: {
+        encrypt: true // Use this if you're on Windows Azure 
+    }
+
+
 };
+
