75ee95096c42b4121cd5439e4403297e84050477
ldrc2895 <ldrc2895@gmail.com>
2016-05-09 01:30:59 -0600

    mas y mas cambios

--- controllers/usuarios_invitados.js
+++ controllers/usuarios_invitados.js
@@ -4,7 +4,7 @@ var queryReturn = require("./queryReturn.js")
 
 exports.updateUsuario_Invitado = function (req, resp, usuario_invitado) {
     var sqlStatement = "update usuarios_invitados set confirmado = "+(usuario_invitado.confirmado==true?1:0)+" "+
-                       ", precio_entradas = " + usuario_invitado.precio_entradas+" "+
+                       ", precio_entradas = " + (usuario_invitado.confirmado == true?usuario_invitado.precio_entradas:0)+" "+
                        "where codigo_usuario = " + usuario_invitado.codigo_usuario + " and codigo_evento = " + usuario_invitado.codigo_evento;
     if (!(usuario_invitado.confirmado)) {
         sqlStatement += " delete acompanantes_usuarios where codigo_usuario = " + usuario_invitado.codigo_usuario + " and codigo_evento = " + usuario_invitado.codigo_evento;
